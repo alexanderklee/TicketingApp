ef30e7789bfe5507e48493e0246e5c6bac9fda50
Alexander Lee <alexander.k.lee@gmail.com>
2020-08-09 13:12:14 -0700

    nats-streaming tests

--- tickets/src/routes/__test__/show.test.ts
+++ tickets/src/routes/__test__/show.test.ts
@@ -16,7 +16,7 @@ it('returns a 404 if the ticket is not found', async () => {
         .send()
         .expect(404);
     
-    console.log(response);
+    // console.log(response);
 });
 
 it('returns the ticket if the ticket is found', async () => {
