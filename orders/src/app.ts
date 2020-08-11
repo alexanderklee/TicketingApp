d22f2fb8ecffd98eede2b938db3c7cfc23311293
Alexander Lee <alexander.k.lee@gmail.com>
2020-08-11 14:18:23 -0700

    adding skeleton order code, updated skaffold and added k8 depl and services order definitions

---
+++ orders/src/app.ts
@@ -0,0 +1,44 @@
+import express from 'express';
+import 'express-async-errors';
+import { json } from 'body-parser';
+import cookieSession from 'cookie-session';
+import { errorHandler, NotFoundError, currentUser } from '@goosenest/common';
+
+import { createTicketRouter } from './routes/new';
+import { showTicketRouter } from './routes/show';
+import { indexTicketRouter } from './routes/index';
+import { updateTicketRouter } from './routes/update';
+
+const app = express();
+// need to let express know its behind ingress-nginx
+app.set('trust proxy', true);
+app.use(json());
+// using cookies and unencrypted jwt due to server-side
+// rendering (Next) implementation
+app.use(
+    cookieSession({
+        signed: false,
+        // Need workaround for jest using unsecured testing
+        // By default, jest uses http so extracting node_env
+        // to flip "secure" value to false while in test mode
+        // and cookieSession / secure set to true will cause
+        // Set-Cookie not to be present.
+        secure: process.env.NODE_ENV !== 'test'
+    })
+);
+
+// wire up middlewares
+app.use(currentUser);
+app.use(createTicketRouter);
+app.use(showTicketRouter);
+app.use(indexTicketRouter);
+app.use(updateTicketRouter);
+
+// capture all invalid routes and invalid http verbs
+app.all('*', async (req,res) => {
+    throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
